ecc385094a17990fa5fb84108ae06f1ad8326d88
sw19_sungpil <philip000412@gmail.com>
2023-01-12 17:51:48 +0900

    01/12

--- acmicpc.net/11049/main.cpp
+++ acmicpc.net/11049/main.cpp
@@ -2,18 +2,6 @@
 using namespace std;
 int main(){
     ios::sync_with_stdio(false), cin.tie(NULL);
-    int i, j, k, n, dy[510][510]={0}, num[510];
-    cin >> n >> num[0];
-    for(i=1;i<=n-1;i++) cin >> num[i] >> num[i];
-    cin >> num[n];
-    for(i=1;i<n;i++){
-        for(j=1;j<=n-i;j++){
-            dy[j][j+i] = 0x7fffffff;
-            for(k=j;k<j+i;k++){
-                dy[j][j+i] = min(dy[j][j+i], num[j-1]*num[k]*num[j+i]+dy[j][k]+dy[k+1][j+i]);
-            }
-        }
-    }
-    cout << dy[1][n];
+    
     return 0;
 }
