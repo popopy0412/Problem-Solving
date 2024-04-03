af2589f957ba8348c6a91dc39c34c2b5cb465339
Philip_Cheon <philip000412@gmail.com>
2024-04-03 22:32:08 +0900

    solved: acmicpc.net/10999

--- acmicpc.net/10999/main.py
+++ acmicpc.net/10999/main.py
@@ -14,28 +14,23 @@ def check(tree, lazy, l, r, v, idx):
     if l != r:
         lazy[idx*2]+=v
         lazy[idx*2+1]+=v
+    lazy[idx]=0
 def query(tree, lazy, l, r, s, e, idx):
-    if lazy[idx]:
-        check(tree, lazy, l, r, lazy[idx], idx)
-        lazy[idx]=0
+    check(tree, lazy, l, r, lazy[idx], idx)
     if r < s or e < l: return 0
     if s <= l and r <= e: return tree[idx]
     m=(l+r)//2
     return query(tree, lazy, l, m, s, e, idx*2)+query(tree, lazy, m+1, r, s, e, idx*2+1)
 def update(tree, lazy, l, r, s, e, v, idx):
-    if lazy[idx]:
-        check(tree, lazy, l, r, lazy[idx], idx)
-        lazy[idx]=0
+    check(tree, lazy, l, r, lazy[idx], idx)
     if r < s or e < l: return
     if s <= l and r <= e:
         check(tree, lazy, l, r, v, idx)
         return
-    if l <= s <= r <= e: tree[idx]+=v*(r-s+1)
-    elif s <= l <= e <= r: tree[idx]+=v*(e-l+1)
-    elif l <= s <= e <= r: tree[idx]+=v*(e-s+1)
     m=(l+r)//2
     update(tree, lazy, l, m, s, e, v, idx*2)
     update(tree, lazy, m+1, r, s, e, v, idx*2+1)
+    tree[idx]=tree[idx*2]+tree[idx*2+1]
     
 def solve():
     n,m,k=inl()
