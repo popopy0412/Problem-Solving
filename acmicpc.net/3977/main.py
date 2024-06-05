5e8027e427ca243a133921bdd863af9f3d9e1ee5
Philip_Cheon <philip000412@gmail.com>
2024-06-05 11:14:39 +0900

    solved: acmicpc.net/3977

---
+++ acmicpc.net/3977/main.py
@@ -0,0 +1,65 @@
+from collections import *
+import sys
+sys.setrecursionlimit(10**6)
+ip=sys.stdin.readline
+z=lambda:[0]*-~n
+il=lambda:[[] for _ in range(n)]
+def dfs(now):
+    global num,cnt
+    s.append(now)
+    ck[now]=parent=num
+    num+=1
+    for next in v[now]:
+        if ck[next]==0: parent=min(parent,dfs(next))
+        elif fin[next]==0: parent=min(parent,ck[next])
+    if ck[now]==parent:
+        while True:
+            t=s.pop()
+            scc[cnt].append(t)
+            idx[t]=cnt
+            if t==now: break
+        cnt+=1
+    fin[now]=1
+    return parent
+for I in range(T:=int(ip())):
+    n,m=map(int,ip().split())
+    num,cnt=1,0
+    s,nv=[],[]
+    v,scc=il(),il()
+    ck,fin,idx,ind=z(),z(),z(),z()
+    scc=[[] for _ in range(n)]
+    for _ in range(m):
+        a,b=map(int,ip().split())
+        v[a].append(b)
+    ip()
+    for i in range(n):
+        if ck[i]==0: dfs(i)
+    for i in range(cnt):
+        t=set()
+        for now in scc[i]:
+            for next in v[now]:
+                if idx[next]!=i:
+                    t.add(idx[next])
+                    ind[idx[next]]+=1
+        nv.append([*t])
+    t=[]
+    for i in range(cnt):
+        if ind[i]==0: t.append(i)
+    if len(t)==1:
+        t=t[-1]
+        ans=1
+        ck,q=z(),deque([t])
+        ck[t]=1
+        while q:
+            now=q.popleft()
+            for next in nv[now]:
+                if ck[next]==0:
+                    q.append(next)
+                    ck[next]=1
+                    ans+=1
+        if ans==cnt:
+            for i in sorted(scc[t]): print(i)
+        else: print('Confused')
+    else: print('Confused')
+    if I!=T-1: print()
+    
