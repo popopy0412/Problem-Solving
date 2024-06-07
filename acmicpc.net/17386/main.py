bf7907a40bbbf15e8cdaf2bd732a98d20f91d1c9
Philip_Cheon <philip000412@gmail.com>
2024-06-07 14:52:04 +0900

    solved: acmicpc.net/17386

---
+++ acmicpc.net/17386/main.py
@@ -0,0 +1,6 @@
+x1,y1,x2,y2=map(int,input().split())
+x3,y3,x4,y4=map(int,input().split())
+ccw=lambda ax,ay,bx,by,cx,cy:(ax-bx)*(ay-cy)-(ax-cx)*(ay-by)
+lap=lambda x1,x2,x3,x4:min(max(x1,x2),max(x3,x4))<max(min(x1,x2),min(x3,x4))
+a,b=ccw(x1,y1,x2,y2,x3,y3)*ccw(x1,y1,x2,y2,x4,y4),ccw(x3,y3,x4,y4,x1,y1)*ccw(x3,y3,x4,y4,x2,y2)
+print(0 if (a>0 or b>0) or a==0 and b==0 and lap(y1,y2,y3,y4)|lap(x1,x2,x3,x4) else 1)
